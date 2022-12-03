7a235dde7475abeae63eee58c7ecefc8ebea5091
buckket <felix@buckket.org>
2022-12-03 14:14:15 +0100

    2022/02

---
+++ 2022/02/b.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// 1 for Rock, 2 for Paper, and 3 for Scissors
+// 0 if you lost, 3 if the round was a draw, and 6 if you won
+
+func main() {
+	// LUT
+	// First index: 0 for Rock, 1 for Paper, and 2 for Scissors
+	// Second index: 0 for Loss, 1 for Draw, and 2 for Win
+	var score_lut = [3][3]int{
+		{0 + 3, 3 + 1, 6 + 2},
+		{0 + 1, 3 + 2, 6 + 3},
+		{0 + 2, 3 + 3, 6 + 1}}
+
+	var move_lut = map[string]int{
+		"A": 0,
+		"B": 1,
+		"C": 2,
+		"X": 0,
+		"Y": 1,
+		"Z": 2}
+
+	var score int
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		moves := strings.Split(scanner.Text(), " ")
+		if len(moves) != 2 {
+			break
+		}
+		score += score_lut[move_lut[moves[0]]][move_lut[moves[1]]]
+	}
+
+	fmt.Printf("Score: %d\n", score)
+}
